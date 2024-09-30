5abe2fc7d0e83cfa3a22bfb54806967bc1c8593f
jswebdevelopers <web2024developers@gmail.com>
2024-09-30 06:01:55 +0300

    registeryourInterst

--- app/components/project/RegisterButton.tsx
+++ app/components/project/RegisterButton.tsx
@@ -3,7 +3,8 @@ import React, { useState } from 'react';
 import ButtonProp from '@/app/components/buttons/ButtonProp';
 import { MdClose } from 'react-icons/md';
 import ContactForm from '../forms/ContactForm';
-
+import Image from 'next/image'; // Import the Next.js Image component
+import samah from '@/assets/images/samah.png'
 const RegisterButton = () => {
     const [showFilterOverlay, setShowFilterOverlay] = useState(false);
 
@@ -31,13 +32,26 @@ const RegisterButton = () => {
                         {/* Content */}
                         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
                             {/* Text Section */}
-                            <div className="flex flex-col justify-center lg:pr-6">
-                                <h1 className="text-2xl font-bold text-gray-800 mb-4">
+                            <div className="flex flex-col justify-center items-center lg:pr-6">
+                           
+
+                                <h1 className="text-2xl font-bold text-gray-800 mb-4 text-center lg:text-left">
                                     The best deals are our expertise – register now.
                                 </h1>
-                                <p className="text-gray-600 mb-2">Samah Faek</p>
-                                <p className="text-gray-600 mb-2">Chief Executive Officer</p>
-                                <p className="text-gray-600">+971 50 376 9694</p>
+                                <p className="text-gray-600 mb-2 text-center lg:text-left">Samah Faek</p>
+                                <p className="text-gray-600 mb-2 text-center lg:text-left">Chief Executive Officer</p>
+                                <p className="text-gray-600 text-center lg:text-left">+971 50 376 9694</p>
+
+                                     {/* Samah's Image */}
+                                     <div className="mb-4">
+                                    <Image
+                                        src={samah}  
+                                        alt="Samah Faek"
+                                        width={150}  // Adjust width
+                                        height={150} // Adjust height
+                                        className="rounded-full object-cover" // Make the image circular
+                                    />
+                                </div>
                             </div>
 
                             {/* Form Section */}
