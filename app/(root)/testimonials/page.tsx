3cd4317b9c27a7744ed826e33b9011bef7e2eb60
jswebdevelopers <web2024developers@gmail.com>
2024-10-01 05:44:49 +0300

    TestimonialCard

--- app/(root)/testimonials/page.tsx
+++ app/(root)/testimonials/page.tsx
@@ -10,22 +10,21 @@ import { LargeTypingText, SmallTitleText } from '@/app/styles/CustomTexts';
 const breadcrumbItems = [
   {
     label: 'Reviews',
-   
   },
-  
 ];
+
 const Page: React.FC = () => {
   return (
     <Container>
-  <div className="p-6 mt-10 ">
-    <Breadcrumb items={breadcrumbItems}/>
-    <div className='p-12'>
-
-    <LargeTypingText title="Why Our Clients Trust Us" textStyles="text-center" />
-    <SmallTitleText title="Discover what our customers are saying about their experiences." textStyles="text-center" />
-</div>
-</div>
-      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="p-6 mt-10">
+        <Breadcrumb items={breadcrumbItems} />
+        <div className='p-4 md:p-12 space-y-6'> {/* Adjusted padding for smaller screens */}
+          <LargeTypingText title="Why Our Clients Trust Us" textStyles="text-center text-2xl md:text-4xl" /> {/* Responsive text size */}
+          <SmallTitleText title="Discover what our customers are saying about their experiences." textStyles="text-center text-sm md:text-lg" /> {/* Responsive text size */}
+        </div>
+      </div>
+      
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 p-4"> {/* Added padding to grid for small devices */}
         {testimonials.map((t, index) => (
           <TestimonialCard
             key={index}
@@ -38,7 +37,7 @@ const Page: React.FC = () => {
         ))}
       </div>
 
-      <ContactSection/>
+      <ContactSection />
     </Container>
   );
 };
