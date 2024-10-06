83f003b39c1c936c40bc619d9ce7f18626766dbd
jswebdevelopers <web2024developers@gmail.com>
2024-10-07 02:08:50 +0300

    comments

--- app/components/testimonial/TestimonialCard.tsx
+++ app/components/testimonial/TestimonialCard.tsx
@@ -13,18 +13,18 @@ interface TestimonialCardProps {
 
 const TestimonialCard: React.FC<TestimonialCardProps> = ({ name, date, testimonialTitle, testimonial, rating }) => {
   return (
-    <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg p-6 sm:p-8 flex flex-col text-gray-800 dark:text-white"> {/* Light and dark background */}
+    <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg p-6 sm:p-8 flex flex-col text-gray-800 dark:text-white"> 
       <div className="flex justify-between items-start">
         <div className="flex items-center">
-          <FaUserCircle className="text-gray-500 dark:text-gray-400 text-4xl mr-3" /> {/* Icon color adjustment for dark mode */}
+          <FaUserCircle className="text-gray-500 dark:text-gray-400 text-4xl mr-3" /> 
           <div>
             <h3 className="text-lg font-semibold">{name}</h3>
-            <p className="text-sm text-gray-600 dark:text-gray-400">{date}</p> {/* Date color adjustment */}
+            <p className="text-sm text-gray-600 dark:text-gray-400">{date}</p> 
             <div className="flex items-center mt-2">
               {[...Array(5)].map((_, index) => (
                 <FaStar
                   key={index}
-                  className={`text-yellow-500 ${index < rating ? 'fill-current' : 'text-gray-300 dark:text-gray-600'}`} // Star color adjustments
+                  className={`text-yellow-500 ${index < rating ? 'fill-current' : 'text-gray-300 dark:text-gray-600'}`} 
                 />
               ))}
             </div>
