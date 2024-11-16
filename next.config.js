204e6ab0e392c5587e00b70e7a43a9dd09f9dc0f
Miss-Maha1997 <mahaali1121997@gmail.com>
2024-11-17 02:16:32 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -18,28 +18,8 @@ const nextConfig = {
   },
 
   
+  productionBrowserSourceMaps: true,
    
-  // Custom environment variable validation
-  webpack(config, { isServer }) {
-    if (isServer) {
-      // Check if required environment variables are set
-      const requiredEnvVars = [
-        'NEXT_PUBLIC_API_URL',
-        'NEXT_PUBLIC_CLOUDINARY_URL',
-        'NEXT_PUBLIC_SANITY_PROJECT_ID',
-        'SENDGRID_API_KEY',
-      ];
+}
 
-      requiredEnvVars.forEach((envVar) => {
-        if (!process.env[envVar]) {
-          console.error(`Error: Missing required environment variable: ${envVar}`);
-          process.exit(1); // Stop build if any required env var is missing
-        }
-      });
-    }
-
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig 
