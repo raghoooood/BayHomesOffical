efdd9f614a7e1017bbeead3a1bc03bca32fc3b12
maha20514 <maha053222@gmail.com>
2024-11-17 21:55:01 +0300

    Remove unwanted files

--- assets/images/indes.tsx
+++
@@ -1,3 +0,0 @@
-import buyGuid from './buy-guid-bg2.jpg'
-import sellGuid from './buy-guid_bg.jpg'
-
--- utils/listingSteps.ts
+++
@@ -1,7 +0,0 @@
-export interface Step {
-    step_number: string;
-    title: string;
-    description: string;
-    icon: React.ReactNode; // Use React nodes for icons
-  }
-  
